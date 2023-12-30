49f1979a607958a7ac44f0c32bcd99449cba3ae6
Shizuka <yosiaondi48@gmail.com>
2023-12-30 15:21:41 +0700

    Update homepage.blade.php

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -63,7 +63,6 @@
                       </div>
                     </div>
                   </div>
-
                   <div class="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
                     <button type="submit"
                       class="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 sm:ml-3 sm:w-auto">Create</button>

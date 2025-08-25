c585453b76dcff5ce7e24301ce33b9daa0818758
Samuel Sihotang <samsihotang1@gmail.com>
2025-08-25 23:18:21 +0700

    Update app.blade.php

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -46,7 +46,7 @@
           </div>
           <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <div class="flex flex-shrink-0 items-center">
-              <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&amp;shade=600"
+              <img class="h-8 w-auto" src="{{ asset('storage/icons8-rice-bowl-96.png') }}"
                 alt="Your Company">
             </div>
             <div class="hidden sm:ml-6 sm:flex sm:space-x-8">
